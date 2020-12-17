05ac0ab588376808d127bbaa5d194212e14ca89a
Diana-nyamai <dnyamai.dn@gmail.com>
2020-12-17 20:12:52 +0300

    changes

---
+++ my-work/src/components/propsInClass.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+ class propsInClass extends Component {
+     
+    render() {
+        return (
+            <div>
+                <h1>my name is {this.props.name} and skilled in {this</h1>
+            </div>
+        )
+    }
+}
+
+export default propsInClass
