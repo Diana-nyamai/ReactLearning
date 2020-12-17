f3fad26dd2ea80362aa373672bc7a00c22282472
Diana-nyamai <dnyamai.dn@gmail.com>
2020-12-17 19:07:26 +0300

    changes

--- my-work/src/App.js
+++ my-work/src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import './App.css';
+import Increment1 from './components/Increment1';
 import OnClickButton from './components/OnClickButton';
 
 
 function App() {
   return (
     <div className="App">
-      <OnClickButton/>
+      {/*<OnClickButton/>*/
+      
+      }
+      <Increment1/>
      </div>
   );
 }
---
+++ my-work/src/components/Increment1.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+
+class Increment1 extends Component {
+    constructor(props) {
+        super(props)
+    
+        this.state = {
+             count:0
+        }
+    }
+    
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default Increment1
