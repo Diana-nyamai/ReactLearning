e128f902da8e8f836ac44fd44f46ec3532c9a9e0
Diana-nyamai <dnyamai.dn@gmail.com>
2020-12-17 18:23:49 +0300

    commit

--- my-work/src/components/OnClickButton.js
+++ my-work/src/components/OnClickButton.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react'
 
 export class OnClickButton extends Component {
+    constructor(props) {
+        super(props)
+    
+        this.state = {
+             message:'welcome Visitor'
+        }
+    }
+    
     render() {
         return (
             <div>
-                
+                <h1>{this.state.message}</h1>
+                <button>click me</button>
             </div>
         )
     }
