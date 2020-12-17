bdd2be7c2b809bfe73f94276220bb609c48fdc2d
Diana-nyamai <dnyamai.dn@gmail.com>
2020-12-17 20:01:33 +0300

    changes

--- my-work/src/components/Props.js
+++ my-work/src/components/Props.js
@@ -1,10 +1,10 @@
 import React from 'react'
 
-const Props = props =>{
+const Props = props => {
     return (
-        <div>
+        
         <h1>my name is {props.name} and good at {props.skill}</h1>
-        </div>
+        
     )
        
     
