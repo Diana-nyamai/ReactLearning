7f15d51b8d7564dbc54df94dcd90fc09a2d17942
Diana-nyamai <dnyamai.dn@gmail.com>
2020-12-17 18:24:23 +0300

    commit

--- my-work/src/App.js
+++ my-work/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
+import OnClickButton from './components/OnClickButton';
 
 
 function App() {
   return (
     <div className="App">
-      
+      <OnClickButton/>
      </div>
   );
 }
