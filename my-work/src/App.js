7fe2aeb105ea972d267782d2353ad1e0449caae3
Diana-nyamai <dnyamai.dn@gmail.com>
2020-12-17 18:05:55 +0300

    first commit

---
+++ my-work/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import ParentComponent from './theyinyan/ParentComponent';
+import Form from './theyinyan/Form';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Form/>
+     <ParentComponent/>
+    </div>
+  );
+}
+
+export default App;
