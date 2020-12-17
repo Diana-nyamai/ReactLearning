050f7da863ab8d042e70a2904b6d6728166a5369
Diana-nyamai <dnyamai.dn@gmail.com>
2020-12-17 18:22:05 +0300

    changes

---
+++ my-work/src/components/OnClickButton.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class OnClickButton extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default OnClickButton
