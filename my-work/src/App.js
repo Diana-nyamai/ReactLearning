4170294fafb183d7199650cf97c9654f17aa57b2
Diana-nyamai <dnyamai.dn@gmail.com>
2021-02-25 13:10:01 +0300

    changes

--- my-work/src/App.js
+++ my-work/src/App.js
@@ -2,23 +2,23 @@ import React from 'react';
 import './App.css';
 import ButtonON from './components/ButtonON';
 import ConditionalRendering from './components/ConditionalRendering';
-//import Props from './components/Props';
-//import  { PropsInClass } from './components/PropsInClass';
+import Props from './components/Props';
+import  { PropsInClass } from './components/PropsInClass';
 import Timer from './components/Timer';
-//import Increment1 from './components/Increment1';
-//import Increment5 from './components/Increment5';
-//import OnClickButton from './components/OnClickButton';
+import Increment1 from './components/Increment1';
+import Increment5 from './components/Increment5';
+import OnClickButton from './components/OnClickButton';
 
 function App() {
   return (
     <div className="App">
-      {/*<OnClickButton/>
+      <OnClickButton/>
          <Increment1/>
         <Increment5/>
          <Props name="diana" skill="react"/> 
          <PropsInClass name="phyllis" skill="python"/>
-         */
-      }
+         
+      
      <Timer/>
      <ButtonON/>
      <ConditionalRendering IsLoggedIn ={false}/>
